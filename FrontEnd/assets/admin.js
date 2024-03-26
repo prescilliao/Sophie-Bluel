a6fe31cab7934d980c7c590a58f1a76cdda6a212
De Oliveira Prescillia <oliveiraprescillia04@gmail.com>
2024-03-26 15:29:45 +0100

    ajout corbeil + fonctionnalite de supression + fermer la modale au click en dehors de la popup + ajout fonctionnalité "ajouter une photo"

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -2,6 +2,24 @@
 
 // si le token est actif
 if (token) {
+  // appel api
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // pour chaque données
+      data.forEach((category) => {
+        // creation element option
+        const option = document.createElement("option");
+        // valeur de option = id de category
+        option.value = category.id;
+        option.innerText = category.name;
+        // option est enfant de de la balise ou on trouve les catgories
+        document.querySelector("#categorie").appendChild(option);
+      });
+    });
+
   // login devient logout
   const logout = document.querySelector(".decoration");
   logout.innerText = "logout";
@@ -12,14 +30,17 @@ if (token) {
   function openModale(modale) {
     // enleve class hidden
     modale.classList.remove("hidden");
-    // recuperation div ou il y a tous les projets ainsi que le bouton ajout photo
+    createWorksMiniGalery(works);
+  }
+
+  function createWorksMiniGalery(worksMini) {
     const popup1MiniGallery = document.querySelector(
       ".modale1 .modale-content .miniGallery"
     );
     // enleve ce quil y a dans
     popup1MiniGallery.innerHTML = "";
-    // pour tout les elements dans la minigalerie
-    works.forEach((work) => {
+    // tous les projets sont dans la minigallerie
+    worksMini.forEach((work) => {
       // affiche projet + ajout d'une corbeil design dans css
       const figure = document.createElement("figure");
       const icone = document.createElement("i");
@@ -27,12 +48,37 @@ if (token) {
       figure.classList.add("position");
       icone.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
       icone.classList.add("trash_icone");
+      // au click sur l'icone corbeille
+      icone.addEventListener("click", (e) => {
+        // supression effet par defaut
+        e.preventDefault();
+        // appel api + appel id de l'element selectionner
+        fetch("http://localhost:5678/api/works/" + work.id, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((response) => {
+          console.log(response);
+          // si response a status 204
+          if (response.status === 204) {
+            // filtrer les projets
+            works = works.filter((workItem) => workItem.id !== work.id);
+            createWorks(works);
+            createWorksMiniGalery(works);
+          }
+          //sinon afficher erreur
+          if (response.status !== 204) {
+            alert("Une erreur s'est produite lors de la supression");
+          }
+          //TODO
+        });
+      });
       img.src = work.imageUrl;
       figure.appendChild(img);
       figure.appendChild(icone);
       popup1MiniGallery.appendChild(figure);
     });
-    // tous les projets sont dans la minigallerie
   }
 
   elementAdmin.forEach((el) => {
@@ -63,53 +109,132 @@ if (token) {
     closeBtn.addEventListener("click", (e) => {
       // enleve event par defaut
       e.preventDefault();
-      // recupere modale 1
-      const popup1 = document.querySelector(".modale1");
-      // ajout class hidden
-      popup1.classList.add("hidden");
-      // recupere modale 2
-      const popup2 = document.querySelector(".modale2");
-      // ajout class hidden
-      popup2.classList.add("hidden");
+      closeModale();
     });
   });
-  // changement de la div en image
-  const Selection = document.getElementById("enteteImage");
-  const PhotoSelectionner = document.getElementById("photoAjouter");
-  const Appercu = document.createElement("img");
-  PhotoSelectionner.addEventListener("click", () => {});
-  console.log(Selection);
-  console.log(PhotoSelectionner);
-  console.log(Appercu);
-  //appel a l'api
-  function ValideProjet() {
-    // methode post
+
+  // fermeture modale au click en dehors de la modale
+  const modaleBackgroud = document.querySelectorAll(".modale");
+  modaleBackgroud.forEach((modale) => {
+    modale.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target === modale) {
+        closeModale();
+      }
+    });
+  });
+
+  function closeModale() {
+    // recupere modale 1
+    const popup1 = document.querySelector(".modale1");
+    // ajout class hidden
+    popup1.classList.add("hidden");
+    // recupere modale 2
+    const popup2 = document.querySelector(".modale2");
+    // ajout class hidden
+    popup2.classList.add("hidden");
+  }
+
+  //au clic ajouter photo
+  //selection de sa photo
+  //afficher la photo en masquant les indication et le bouton
+  var imageValide = false;
+  var titleValide = false;
+
+  document.querySelector("#addpicture").addEventListener("change", (e) => {
+    e.preventDefault();
+    //recupere l'image
+    //e.target.files.item(0) === e.target.files[0]
+    const imageFile = e.target.files[0];
+    //creer un objet url via la fonction URL.createObjectURL
+    const imageUrl = URL.createObjectURL(imageFile);
+
+    // creer une balise img
+    let img = document.createElement("img");
+    // source img
+    img.src = imageUrl;
+    img.classList.add("heightPicture");
+    // placement dynanique de l'img dans div
+    document.querySelector("#showPicture").appendChild(img);
+    //masque les element
+    document.querySelector("#enteteImage").classList.add("hidden");
+    document.querySelector("#showPicture").classList.remove("hidden");
+    imageValide = true;
+    activateButton();
+  });
+
+  document.querySelector("#titre").addEventListener("change", (e) => {
+    e.preventDefault();
+    if (e.target.value === "") {
+      // si champ de text est vide alors est faux
+      titleValide = false;
+      // si non vrai
+    } else {
+      titleValide = true;
+    }
+    activateButton();
+  });
+
+  //faire devenir le bouton valider vert
+  function activateButton() {
+    // si image et titre valide alors
+    if (imageValide && titleValide) {
+      // bouton actif
+      document.querySelector("#check").disabled = false;
+      document.querySelector("#check").classList.remove("disabled");
+    } else {
+      // si non bouton desactiver
+      document.querySelector("#check").disabled = true;
+      document.querySelector("#check").classList.add("disabled");
+    }
+  }
+  //si les champ sont valide activer le bouton au clique sinon ne rien faire
+
+  document.querySelector("#check").addEventListener("click", (e) => {
+    e.preventDefault();
+    // formData permet de construitre un ensemble de paire cle (les champs du formulaire)
+    const formData = new FormData();
+    //recupere les valeurs
+    formData.append("title", document.querySelector("#titre").value);
+    // le numéro 123456 est converti immédiatement en chaîne "123456"
+    formData.append("category", document.querySelector("#categorie").value);
+    formData.append("image", document.querySelector("#addpicture").files[0]);
+    //envoie les valeurs a l'api
     fetch("http://localhost:5678/api/works", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({
-        // envoie des données saisie dans le formulaire a lapi
-        Image: document.getElementById("addpicture").value,
-        title: document.getElementById("titre").value,
-        category: document.getElementById("categorie").value,
-      }),
+      body: formData,
     })
-      // conversion de la reponse
       .then((response) => {
-        return response.json();
+        //si l'api repond ok
+        if (response.status === 201) {
+          return response.json();
+        }
+        //sinon afficher une erreur
+        else {
+          alert("erreur");
+        }
+        //TODO
       })
-      // traitement
       .then((data) => {
-        console.log(data);
+        //ajouter l'image dans les deux galeries
+        works.push(data);
+        createWorks(works);
+        createWorksMiniGalery(works);
+        closeModale();
+        //vide le formulaire
+        document.querySelector("#formAddPicture").reset();
+        document.querySelector("#enteteImage").classList.remove("hidden");
+        document.querySelector("#showPicture").classList.add("hidden");
+        document.querySelector("#showPicture").innerHTML = "";
       });
-  }
+  });
 
-  // quand on clique sur une corbeille supprimer un element
-  ///si api di ok on supprime
-  //supprimer element dans la mini galerie et dans la galerie globale
-  //sinon erreur
+  // un fichier image est selectionner
+  // un titre est saisie
+  // une categorie est selectionner
 
   // recupere bouton ajouter une photo
   const addPictureModale = document.querySelector(".AddPictureModale");
